d97abf089bfb3fb1d8728221956375875b2ff5fc
rhog23 <raymonde.saragih@gmail.com>
2023-02-21 14:49:08 +0700

    update resizing

--- p05/03.1-resizing-opencv.py
+++ p05/03.1-resizing-opencv.py
@@ -8,34 +8,63 @@ def run(image=""):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     while True:
-        print("Resizing Type:\na: percentage\nb: fixed")
-        resize_type = input("Enter resizing type (a | b):").lower()
+        print("Resizing Type:\na: percentage\nb: fixed\nq: quit")
+        resize_type = input("Enter resizing type: ").lower()
 
         if resize_type == "a":
-            print(f"{'='*5} Percentage {'='*5}")
+            print(f"{'='*24} Percentage {'='*24}")
             print("percentage < 100% will downscale the image")
             print("percentage = 100% doesn't change the image size")
             print("percentage > 100% will upscales the image")
-            print(f"{'='*20}")
-            percentage = int(input("Enter resizing percentage (integer): "))
-            width = int(img.shape[1] * percentage / 100)
-            height = int(img.shape[0] * percentage / 100)
-            dimension = (width, height)
+            print(f"{'='*60}")
+            try:
+                percentage = int(input("Enter resizing percentage (integer): "))
+                width = int(img.shape[1] * percentage / 100)
+                height = int(img.shape[0] * percentage / 100)
+                dimension = (width, height)
 
-            result = cv2.resize(img, dimension)
-            print(
-                f"Original Image Shape (width * height): {img.shape[1]} * {img.shape[0]}"
-            )
-            print(
-                f"Resulting Image Shape (width * height): {result.shape[1]} * {result.shape[0]}"
-            )
-            show_result(img, result)
+                result = cv2.resize(img, dimension)
+                print(
+                    f"Original Image Shape (width * height): {img.shape[1]} * {img.shape[0]}"
+                )
+                print(
+                    f"Resulting Image Shape (width * height): {result.shape[1]} * {result.shape[0]}"
+                )
+                show_result(img, result)
+                return False
+            except Exception as e:
+                print(f"Error: {e}")
+
+        if resize_type == "b":
+            print(f"{'='*10} Fixed {'='*10}")
+            try:
+                width = int(input("Enter width: "))
+                height = int(input("Enter heigt: "))
+
+                if width == 0:
+                    width = img.shape[1]
+
+                if height == 0:
+                    height = img.shape[0]
+
+                dimension = (width, height)
+
+                result = cv2.resize(img, dimension)
+                print(
+                    f"Original Image Shape (width * height): {img.shape[1]} * {img.shape[0]}"
+                )
+                print(
+                    f"Resulting Image Shape (width * height): {result.shape[1]} * {result.shape[0]}"
+                )
+                show_result(img, result)
+                return False
+            except Exception as e:
+                print(f"Error: {e}")
+
+        if resize_type == "q":
             return False
-        elif resize_type == "b":
-            print("Fixed")
-            return False
-        else:
-            print("Invalid input. Please enter resizing type (a | b)")
+
+        print("Invalid input. Please enter resizing type")
 
 
 def show_result(orig_img, result):
