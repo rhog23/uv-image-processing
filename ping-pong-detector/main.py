0aa409fb37549983055aa90e959e39d62bdc5050
rhog23 <raymonde.saragih@gmail.com>
2024-05-06 14:59:56 +0700

    [info] update code

--- ping-pong-detector/main.py
+++ ping-pong-detector/main.py
@@ -1,9 +1,32 @@
 import cv2
-from numpy import uint8
+import time, sys
+import picar
+from pymata4 import pymata4
 from ultralytics import YOLO
+
+# Ultrasonic Sensor's Pin
+trigger_pin = 8  #  digital input 8
+echo_pin = 9  #  digital input 9
+
+# Servo's Pin
+servo_pin = 10
+distance = 100  # initial distance is set to 30 cm
+
+# Wheel's Pin
+left_motor_FW = 7
+left_motor_BW = 6
+right_motor_FW = 5
+right_motor_BW = 4
+
+motor = [left_motor_FW, left_motor_BW, right_motor_FW, right_motor_BW]
+
+board = pymata4.Pymata4()
 
 
 if __name__ == "__main__":
+
+    picar.setup(board, trigger_pin, echo_pin, servo_pin, motor)
+
     model = YOLO(
         "ping-pong-detector/models/pingpong-tflite/pingpong-det-small_int8.tflite",
         task="detect",
@@ -13,35 +36,69 @@ if __name__ == "__main__":
 
     while True:
         _, frame = cap.read()
+        right_distance = 0
+        left_distance = 0
+        time.sleep(0.1)
+
+        distance = picar.get_distance(board, trigger_pin)
 
         results = model(frame, imgsz=160, max_det=1, conf=0.5)
 
-        for result in results:
-            for box in result.boxes:
-                coords = box.xyxy
+        if distance <= 45:
+            picar.stop_motor(board, motor)
+            time.sleep(0.1)
+            picar.move_backward(board, motor)
+            time.sleep(0.5)
+            picar.stop_motor(board, motor)
+            time.sleep(0.5)
 
-                if len(coords) > 0:
-                    x = int(coords[0][0])
-                    y = int(coords[0][1])
-                    w = int(coords[0][2])
-                    h = int(coords[0][3])
+            right_distance = picar.look_right(board, trigger_pin, servo_pin)
+            time.sleep(0.5)
+            left_distance = picar.look_left(board, trigger_pin, servo_pin)
+            time.sleep(0.5)
 
-                    cv2.rectangle(frame, (x, y), (w, h), (0, 255, 255), 2)
+            if right_distance > left_distance:
+                picar.turn_right(board, motor)
+                picar.stop_motor(board, motor)
 
-                    cv2.putText(
-                        frame,
-                        f"{result.names[int(box.cls)]} | {box.conf[0]:.2%}",
-                        (x, y),
-                        cv2.FONT_HERSHEY_PLAIN,
-                        1,
-                        (0, 255, 255),
-                        1,
-                        cv2.LINE_AA,
-                    )
-                else:
-                    continue
+            elif right_distance < left_distance:
+                picar.turn_left(board, motor)
+                picar.stop_motor(board, motor)
 
-            cv2.imshow("detection result", frame)
+            else:
+                picar.move_forward(board, motor)
+        else:
+            picar.move_forward(board, motor)
+
+        if results:
+            for result in results:
+                for box in result.boxes:
+                    coords = box.xyxy
+
+                    if len(coords) > 0:
+                        x = int(coords[0][0])
+                        y = int(coords[0][1])
+                        w = int(coords[0][2])
+                        h = int(coords[0][3])
+
+                        cv2.rectangle(frame, (x, y), (w, h), (0, 255, 255), 2)
+
+                        cv2.putText(
+                            frame,
+                            f"{result.names[int(box.cls)]} | {box.conf[0]:.2%}",
+                            (x, y),
+                            cv2.FONT_HERSHEY_PLAIN,
+                            1,
+                            (0, 255, 255),
+                            1,
+                            cv2.LINE_AA,
+                        )
+                    else:
+                        continue
+
+                cv2.imshow("detection result", frame)
+
+            picar.stop_motor(board, motor)
 
         if cv2.waitKey(1) == ord("q"):
             break
---
+++ ping-pong-detector/picar.py
@@ -0,0 +1,100 @@
+import time
+
+
+def setup(board, trigger_pin, echo_pin, servo_pin, motor) -> None:
+    print("[info] sets up sensors and motors")
+    left_motor_FW, left_motor_BW, right_motor_FW, right_motor_BW = motor
+    # sets up ultrasonic
+    board.set_pin_mode_sonar(trigger_pin, echo_pin)
+
+    # sets up servo
+    board.set_pin_mode_servo(servo_pin)
+    board.servo_write(servo_pin, 115)  # align the servo to 115°
+
+    # sets up wheels
+    board.set_pin_mode_digital_output(left_motor_FW)
+    board.set_pin_mode_digital_output(left_motor_BW)
+    board.set_pin_mode_digital_output(right_motor_FW)
+    board.set_pin_mode_digital_output(right_motor_BW)
+
+
+def get_distance(board, trigger_pin):
+    time.sleep(0.07)
+    distance, _ = board.sonar_read(trigger_pin)
+
+    return distance
+
+
+def look_right(board, trigger_pin, servo_pin):
+    board.servo_write(servo_pin, 50)
+    time.sleep(0.5)
+    right_distance = get_distance(board, trigger_pin)
+    print(f"[info] look right | distance: {right_distance}")
+    time.sleep(0.1)
+    board.servo_write(servo_pin, 115)
+
+    return right_distance
+
+
+def look_left(board, trigger_pin, servo_pin):
+    board.servo_write(servo_pin, 170)
+    time.sleep(0.5)
+    left_distance = get_distance(board, trigger_pin)
+    print(f"[info] look left | distance: {left_distance}")
+    time.sleep(0.1)
+    board.servo_write(servo_pin, 115)
+
+    return left_distance
+
+
+def move_forward(board, motor) -> None:
+    print("[info] moving forward")
+    left_motor_FW, left_motor_BW, right_motor_FW, right_motor_BW = motor
+    board.digital_pin_write(left_motor_FW, 1)
+    board.digital_pin_write(right_motor_FW, 1)
+    board.digital_pin_write(left_motor_BW, 0)
+    board.digital_pin_write(right_motor_BW, 0)
+
+
+def stop_motor(board, motor) -> None:
+    print("[info] stop")
+    left_motor_FW, left_motor_BW, right_motor_FW, right_motor_BW = motor
+    board.digital_pin_write(left_motor_FW, 0)
+    board.digital_pin_write(right_motor_FW, 0)
+    board.digital_pin_write(left_motor_BW, 0)
+    board.digital_pin_write(right_motor_BW, 0)
+
+
+def move_backward(board, motor) -> None:
+    print("[info] moving backward")
+    left_motor_FW, left_motor_BW, right_motor_FW, right_motor_BW = motor
+    board.digital_pin_write(left_motor_FW, 0)
+    board.digital_pin_write(right_motor_FW, 0)
+    board.digital_pin_write(left_motor_BW, 1)
+    board.digital_pin_write(right_motor_BW, 1)
+
+
+def turn_left(board, motor) -> None:
+    print("[info] turns left")
+    left_motor_FW, left_motor_BW, right_motor_FW, right_motor_BW = motor
+    board.digital_pin_write(left_motor_FW, 1)
+    board.digital_pin_write(right_motor_BW, 1)
+
+    board.digital_pin_write(left_motor_BW, 0)
+    board.digital_pin_write(right_motor_FW, 0)
+
+    time.sleep(0.5)
+    stop_motor(board, motor)
+
+
+def turn_right(board, motor) -> None:
+    print("[info] turns right")
+    left_motor_FW, left_motor_BW, right_motor_FW, right_motor_BW = motor
+    board.digital_pin_write(right_motor_FW, 1)
+    board.digital_pin_write(left_motor_BW, 1)
+
+    board.digital_pin_write(left_motor_FW, 0)
+    board.digital_pin_write(right_motor_BW, 0)
+
+    time.sleep(0.5)
+    stop_motor(board, motor)
